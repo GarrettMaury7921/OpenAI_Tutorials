103c688ff2dec62ffaaf7ab88d854bc4d73bd523
Garrett Maury <jgmaury7921@gmail.com>
2021-10-21 23:12:41 -0400

    Working Deep Learning CustomEnvironment.py

--- CustomEnvironment.py
+++ CustomEnvironment.py
@@ -9,8 +9,8 @@ from rl.agents import DQNAgent
 from rl.policy import BoltzmannQPolicy
 from rl.memory import SequentialMemory
 
+
 class ShowerEnv(Env):
-    # Called at the beginning and does everything you want it to do at the start
     def __init__(self):
         # Actions we can take, down, stay, up
         self.action_space = Discrete(3)
@@ -63,8 +63,8 @@ class ShowerEnv(Env):
 
 
 env = ShowerEnv()
+
 env.observation_space.sample()
-episodes = 10
 
 states = env.observation_space.shape
 actions = env.action_space.n
@@ -85,13 +85,13 @@ def build_agent(model, actions):
     policy = BoltzmannQPolicy()
     memory = SequentialMemory(limit=50000, window_length=1)
     dqn = DQNAgent(model=model, memory=memory, policy=policy,
-                  nb_actions=actions, nb_steps_warmup=10, target_model_update=1e-2)
+                   nb_actions=actions, nb_steps_warmup=10, target_model_update=1e-2)
     return dqn
 
 
 dqn = build_agent(model, actions)
 dqn.compile(Adam(lr=1e-3), metrics=['mae'])
-dqn.fit(env, nb_steps=50000, visualize=False, verbose=1)
+# dqn.fit(env, nb_steps=50000, visualize=False, verbose=1)
+
 scores = dqn.test(env, nb_episodes=100, visualize=False)
 print(np.mean(scores.history['episode_reward']))
-_ = dqn.test(env, nb_episodes=15, visualize=True)
