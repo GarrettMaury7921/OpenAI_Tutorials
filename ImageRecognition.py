3e652ffdfe75310c98ca5cb219c951b51c49c9b5
Garrett Maury <jgmaury7921@gmail.com>
2021-10-22 12:33:59 -0400

    Image Recognition Working
    - Uses Images In Directory
    - Only says it can see if it's 90% sure it's the image

--- ImageRecognition.py
+++ ImageRecognition.py
@@ -6,10 +6,16 @@ import random
 import win32api
 import win32con
 
-while True:
-    if pyautogui.locateOnScreen('image_assets/main_menu') is not None:
+x = 0
+while 1:
+    # MAY CHANGE IF YOU CHANGE COMPUTERS
+    # MUST BE SHIFT + (Windows Key) + S FOR SCREENSHOT
+    if pyautogui.locateOnScreen('image_assets/main_menu.png', confidence=.9) is not None:
         print('I can see it!')
         time.sleep(0.5)
+        break
     else:
         print('I cannot see it...')
+        print(x)
+        x = x+1
         time.sleep(0.5)
