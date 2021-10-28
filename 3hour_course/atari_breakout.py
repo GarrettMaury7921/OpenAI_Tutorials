52f107828a186112fbc6be9d5a281fe0a45cbeac
Garrett Maury <jgmaury7921@gmail.com>
2021-10-28 15:20:38 -0400

    Atari Breakout with the ability to load current models and continue to learn those models

--- 3hour_course/atari_breakout.py
+++ 3hour_course/atari_breakout.py
@@ -5,9 +5,11 @@ from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.env_util import make_atari_env
 import os
 
+# PATHS OF FILES
 log_path = os.path.join('Training', 'Logs')
 a2c_path = os.path.join('Training', 'Saved Models', 'A2C_model')
 a2c_2m_path = os.path.join('Training', 'Saved Models', 'A2C_2M_model')
+a2c_2more_path = os.path.join('Training', 'Saved Models', 'A2C_2M+_model')
 
 # TESTING
 env = make_atari_env('Breakout-v0', n_envs=1, seed=0)
@@ -18,7 +20,9 @@ env = VecFrameStack(env, n_stack=4)
 # env = VecFrameStack(env, n_stack=4)
 
 # Model
-model = A2C("CnnPolicy", env, verbose=1, tensorboard_log=log_path)
+# model = A2C("CnnPolicy", env, verbose=1, tensorboard_log=log_path)
+# Without logging
+model = A2C("CnnPolicy", env, verbose=1)
 
 # LEARNING
 # model.learn(total_timesteps=400000)
@@ -30,10 +34,15 @@ model = A2C("CnnPolicy", env, verbose=1, tensorboard_log=log_path)
 # LOAD MODEL
 # model = A2C.load(a2c_path, env)
 # 2 Million Tries Model
-model = A2C.load(a2c_2m_path, env)
+model = A2C.load(a2c_2more_path, env)
+
+# LEARN A LOADED MODEL
+# model.learn(total_timesteps=20000)
+# SAVE MODEL
+# model.save(a2c_2more_path)
 
 # EVALUATE AND TEST
-# evaluate_policy(model, env, n_eval_episodes=10, render=True)
+evaluate_policy(model, env, n_eval_episodes=10, render=True)
 obs = env.reset()
 while True:
     action, _states = model.predict(obs)
