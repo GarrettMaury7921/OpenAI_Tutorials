82b935499793352b92414445b719639877411ee7
Garrett Maury <jgmaury7921@gmail.com>
2021-12-26 18:59:36 -0500

    Directory Changes + Cascade Classifier Starting Code
    - Training an OpenCV Cascade Classifier to find objects

--- Pre-Swim_Bot/WindowCapture/main.py
+++ Pre-Swim_Bot/WindowCapture/main.py
@@ -9,7 +9,7 @@ wincap = WindowCapture()
 # initialize the Vision class
 vision_limestone = Vision('images/thumb.jpg')
 # initialize the trackbar window
-vision_limestone.init_control_gui()
+# vision_limestone.init_control_gui()
 
 # example limestone HSV filter
 # hsv_filter = HsvFilter(0, 180, 129, 15, 229, 243, 143, 0, 67, 0)
@@ -20,17 +20,17 @@ while True:
     # get an updated image of the game
     screenshot = wincap.get_screenshot()
 
-    # pre-process the image
-    processed_image = vision_limestone.apply_hsv_filter(screenshot)
-
-    # object detection
-    rectangles = vision_limestone.find(processed_image, 0.5)
-
-    # draw the detection results onto the original image
-    output_image = vision_limestone.draw_rectangles(screenshot, rectangles)
+    # # pre-process the image
+    # processed_image = vision_limestone.apply_hsv_filter(screenshot)
+    #
+    # # object detection
+    # rectangles = vision_limestone.find(processed_image, 0.5)
+    #
+    # # draw the detection results onto the original image
+    # output_image = vision_limestone.draw_rectangles(screenshot, rectangles)
 
     # resize the windows
-    output_image = cv.resize(output_image, (900, 500))
+    output_image = cv.resize(screenshot, (900, 500))
     # processed_image = cv.resize(processed_image, (900, 500))
 
     # Display the processed image and
@@ -43,8 +43,13 @@ while True:
 
     # press 'q' with the output window focused to exit.
     # waits 1 ms every loop to process key presses
-    if cv.waitKey(1) == ord('q'):
+    key = cv.waitKey(1)
+    if key == ord('q'):
         cv.destroyAllWindows()
         break
+    elif key == ord('z'):
+        cv.imwrite('AI_Cascade_Classifier/data/positive/{}.jpg'.format(loop_time), screenshot)
+    elif key == ord('x'):
+        cv.imwrite('AI_Cascade_Classifier/data/negative/{}.jpg'.format(loop_time), screenshot)
 
 print('Done.')
